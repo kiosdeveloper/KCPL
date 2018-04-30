a26da7e6c1315169309356ff924e982c5460ae0f
Krishn <krishn@github.io>
2018-05-01 00:41:08 +0530

    Initial commit.

---
+++ Pods/Target Support Files/Pods-KCPLTests/Pods-KCPLTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_KCPLTests : NSObject
+@end
+@implementation PodsDummy_Pods_KCPLTests
+@end
---
+++ Pods/Target Support Files/Pods-KCPLUITests/Pods-KCPLUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_KCPLUITests : NSObject
+@end
+@implementation PodsDummy_Pods_KCPLUITests
+@end
